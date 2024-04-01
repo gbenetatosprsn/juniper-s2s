a9e1c0f75d0ca96cc94bd530321ff781742d8def
Snail Mail <>
2024-04-01 11:47:20 -0700

    all

--- app.py
+++ app.py
@@ -198,6 +198,6 @@ set routing-options static route {cidr} next-hop st0.{log1}
     return render_template('result.html', config=config)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5001)
 
 
